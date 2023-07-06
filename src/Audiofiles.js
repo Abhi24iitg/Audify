13ce359628682a24070f63a657c6639a37d6baac
Abhishek Gupta <abhishek24092003gupta@gmail.com>
2023-07-06 15:28:26 +0530

    improved css and fetch data

--- src/Audiofiles.js
+++ src/Audiofiles.js
@@ -1,30 +1,32 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 function Audios() {
   const [audioData, setAudioData] = useState(null);
-  const [audioUrl, setAudioUrl] = useState('');
+  const [audioUrl, setAudioUrl] = useState("");
 
-  // useEffect(() => {
-  //   fetchAudioFiles();
-  // }, []);
-  const email=JSON.parse(localStorage.getItem('useraudify')).email;
+  useEffect(() => {
+    fetchAudioFiles();
+  }, []);
+  const email = JSON.parse(localStorage.getItem("useraudify")).email;
   const fetchAudioFiles = async () => {
     try {
-      const response = await fetch(`http://localhost:5000/audify?email=${email}`);
+      const response = await fetch(
+        `http://localhost:5000/audify?email=${email}`
+      );
       if (response.ok) {
         const data = await response.json();
-        console.log(data.audio.audio.data);
-        setAudioData(data.audio.audio.data);
+        console.log(data);
+        setAudioData(data);
       } else {
-        console.error('Failed to fetch audio files:', response.statusText);
+        console.error("Failed to fetch audio files:", response.statusText);
       }
     } catch (error) {
-      console.error('Failed to fetch audio files:', error);
+      console.error("Failed to fetch audio files:", error);
     }
   };
   useEffect(() => {
     if (audioData) {
-      const blob = new Blob([audioData], { type: 'audio/mp3' });
+      const blob = new Blob([audioData], { type: "audio/mp3" });
       const url = URL.createObjectURL(blob);
       setAudioUrl(url);
     }
@@ -32,9 +34,9 @@ function Audios() {
 
   return (
     <>
-    <div>
-      <h1>Audio Files</h1>
-      {/* {audioFiles.map((audio) => (
+      <div>
+        <h1>Audio Files</h1>
+        {/* {audioFiles.map((audio) => (
         <div key={audio._id}>
           <h2>{audio.filename}</h2>
           <audio controls>
@@ -42,14 +44,13 @@ function Audios() {
           </audio>
         </div>
       ))} */}
-      {audioUrl && (
-        <audio controls>
-          <source src={audioUrl} type="audio/mpeg" />
-          Your browser does not support the audio element.
-        </audio>
-      )}
-    
-    </div>
+        {audioUrl && (
+          <audio controls>
+            <source src={audioUrl} type="audio/mpeg" />
+            Your browser does not support the audio element.
+          </audio>
+        )}
+      </div>
     </>
   );
 }
--- src/Home.js
+++ src/Home.js
@@ -1,39 +1,21 @@
-import React,{useEffect,useState} from "react";
+import React from "react";
 import Audiolist from "./Audiolist";
 import "./home.css";
-import useFetch from "./useFetch";
-const Home = (props) => {
- 
-   
- const [audiofiles,setAudiofiles]=useState([]);
- const email=JSON.parse(localStorage.getItem('useraudify')).email;
- useEffect(()=>{
-  fetch(`http://localhost:5000/audify?email=${email}`).then(res=>res.json()).then(data=>{
-  console.log(data.audios[0].audioArray);  
-  setAudiofiles(data.audios[0].audioArray)});
- },[])
-  // const handleDelete = (id) => {
-  //   const newAudios = audios.filter((audio) => audio.id !== id);
-  //   setaudios(newAudios);
-  // };
-  return (
-    <>
-      <div style={props.blur}>
-        <div className="home" style={props.home_style}>
 
-          {audiofiles.audioArray && 
-            <Audiolist
-              audios={props.audiofiles}
-              // handleDelete={handleDelete}
-              wel_style={props.wel_style}
-              my_style={props.my_style}
-              title={props.title}
-              popupfun={props.popupfun}
-            />
-          }
-        </div>
+const Home = (props) => {
+  return (
+    <div style={props.blur}>
+      <div className="home" style={props.home_style}>
+        {
+          <Audiolist
+            wel_style={props.wel_style}
+            my_style={props.my_style}
+            title={props.title}
+            popupfun={props.popupfun}
+          />
+        }
       </div>
-    </>
+    </div>
   );
 };
 
